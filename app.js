bc636c493124d7be6e91fc3498329f66f719dadc
Michael Greco <michael.greco07@gmail.com>
2020-07-07 11:14:13 -0400

    Bronze 1

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-// Write a for loop that prints to the console the numbers 0 through 9.
+// ! Write a for loop that prints to the console the numbers 0 through 9.
 
 // for (var i = 0; i <= 9; i++) {
 //   console.log(i);
 // }
 
-// Write a for loop that prints to the console 9 through 0.
+// ! Write a for loop that prints to the console 9 through 0.
 
 // for (var i = 9; i >= 0; i--) {
 //   console.log(i);
 // }
 
-// Write a for loop that prints these fruits to the console.
-var fruit = ['banana', 'orange', 'apple', 'kiwi'];
+// ! Write a for loop that prints these fruits to the console.
 
-for (var i = 0; i < fruit.length; i++) {
-  console.log(fruit[i]);
+// var fruit = ['banana', 'orange', 'apple', 'kiwi'];
+// for (var i = 0; i < fruit.length; i++) {
+//   console.log(fruit[i]);
+// }
+
+// ! Write a for loop that will push the numbers 0 through 9 to an array.
+
+var numberArray = [];
+for (var i = 0; i <= 9; i++) {
+  numberArray.push(i);
 }
+console.log(numberArray);
