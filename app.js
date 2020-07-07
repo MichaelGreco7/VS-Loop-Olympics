97e4f8a6ba20595a00885282fa17dd8ab2e8298e
Michael Greco <michael.greco07@gmail.com>
2020-07-07 10:56:17 -0400

    preliminarie 2

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // Write a for loop that prints to the console the numbers 0 through 9.
 
-for (var i = 0; i <= 9; i++) {
+// for (var i = 0; i <= 9; i++) {
+//   console.log(i);
+// }
+
+// Write a for loop that prints to the console 9 through 0.
+
+for (var i = 9; i >= 0; i--) {
   console.log(i);
 }
